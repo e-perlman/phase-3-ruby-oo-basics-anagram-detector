7b6be37972af1e92ba7a04efb955ca2fa572af75
Emma Perlman <emmajperlman@gmail.com>
2022-06-06 22:37:50 -0700

    commit tree e24edbfd163654dd2d5a1c0a805fb0b16fdf9913

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    def initialize(word)
+        @word=word
+    end
+    def match(anagrams)
+        sorted=anagrams.map {|word| word.sort}
+        .filter do |word|
+            word.size==@word.size
+        end
+        .filter do |word|
+            word.include?@word
+        end
+
+    end
+end
+poo=Anagram.new("listen")
+
